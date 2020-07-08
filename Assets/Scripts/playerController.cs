3da15ecc62c03766236ba658ff878e65c94bc307
Jraxen1 <67929168+Jraxen1@users.noreply.github.com>
2020-07-08 14:40:31 -0700

    Add files via upload

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    // Speed
+	public float speedMult;
+
+	Vector2 cVec;
+
+	Rigidbody2D rb;
+
+	public float jumpForce;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+		if (Input.GetAxisRaw("Horizontal")!=0)
+		{
+			rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speedMult, rb.velocity.y);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			rb.velocity += new Vector2(0, jumpForce);
+		}
+    }
+
+
+
+}
