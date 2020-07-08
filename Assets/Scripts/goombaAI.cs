055051076205c476534f5deffb04d0213b7aa79d
Jraxen1 <67929168+Jraxen1@users.noreply.github.com>
2020-07-08 15:38:54 -0700

    Add files via upload

---
+++ Assets/Scripts/goombaAI.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goombaAI : MonoBehaviour
+{
+	public enum enemies {goomba, gShell}
+	public enemies cEnemy;
+
+	public Sprite[] eSprites;
+
+	public enum states {moveLeft, moveRight};
+	states cState = states.moveLeft;
+
+	public float speed;
+
+	Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		rb = GetComponent<Rigidbody2D>();
+        if(cEnemy == enemies.goomba)
+		{
+			GetComponent<SpriteRenderer>().sprite = eSprites[0];
+		} else if(cEnemy == enemies.gShell)
+		{
+			GetComponent<SpriteRenderer>().sprite = eSprites[1];
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(cEnemy == enemies.goomba)
+		{
+            if(cState == states.moveLeft)
+			{
+				rb.velocity = new Vector2(-speed, rb.velocity.y);
+			} else if(cState == states.moveRight)
+			{
+				rb.velocity = new Vector2(speed, rb.velocity.y);
+			}
+		} else if(cEnemy == enemies.gShell)
+		{
+			if (cState == states.moveLeft)
+			{
+				rb.velocity = new Vector2(-speed, rb.velocity.y);
+			}
+			else if (cState == states.moveRight)
+			{
+				rb.velocity = new Vector2(speed, rb.velocity.y);
+			}
+		}
+    }
+
+    public void die()
+	{
+        if(cEnemy == enemies.goomba)
+		{
+			Destroy(this.gameObject);
+		} else if(cEnemy == enemies.gShell)
+		{
+			// make shell
+			Destroy(this.gameObject);
+		}
+	}
+
+
+    void OnCollisionEnter2D(Collision2D col)
+	{
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Ground")
+		{
+            if(cEnemy == enemies.goomba || cEnemy == enemies.gShell)
+			{
+				if (cState == states.moveLeft)
+				{
+					cState = states.moveRight;
+				}
+				else if (cState == states.moveRight)
+				{
+					cState = states.moveLeft;
+				}
+			}
+			
+		}
+	}
+
+
+}
---
+++ Assets/qBox.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class qBox : MonoBehaviour
+{
+	public enum brickTypes {qBox,brick}
+	public brickTypes cBrick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void die()
+	{
+        if(cBrick == brickTypes.qBox)
+		{
+			cBrick = brickTypes.brick;
+            // Make object
+		}
+	}
+
+
+    void OnTriggerEnter2D(Collider2D col)
+	{
+        if(col.gameObject.tag == "Player")
+		{
+			//print("bop");
+            if(cBrick == brickTypes.qBox)
+			{
+				die();
+			}
+		}
+	}
+}
